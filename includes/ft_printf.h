40c9c4c156d7bdf054e6bc95bae6ccd45ee9af5f
Alan <alan_pires2@hotmail.com>
2021-07-20 15:49:21 -0300

    norm...

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: apires-d <apires-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 19:58:33 by apires-d          #+#    #+#             */
-/*   Updated: 2021/07/20 15:02:34 by apires-d         ###   ########.fr       */
+/*   Updated: 2021/07/20 15:48:09 by apires-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,19 @@ typedef struct s_option
 	char	hexa;
 }			t_option;
 
-int			ft_printf(const char *format, ...);
-void		ft_printf_char(va_list args, t_option *opt);
-void		ft_printf_str(va_list args, t_option *opt);
-void		ft_printf_int(va_list args, t_option *opt);
-void		ft_printf_unsi_int(va_list args, t_option *opt);
-void		ft_printf_unsi_hex(va_list args, t_option *opt);
-void		ft_printf_point(va_list args, t_option *opt);
-void		ft_printf_percent(t_option *opt);
-void		ft_check_conversion(const char *format, va_list args, t_option *opt);
-void		ft_putnbr_count(int n, t_option *opt);
-void		ft_putstr_count(const char *s, t_option *opt);
-void		ft_putchar_count(char c, t_option *opt);
-void		ft_put_unsi_count(unsigned int n, t_option *opt);
-void		ft_reset_opt(t_option *opt);
-void		init_options(t_option *opt);
+int		ft_printf(const char *format, ...);
+void	ft_printf_char(va_list args, t_option *opt);
+void	ft_printf_str(va_list args, t_option *opt);
+void	ft_printf_int(va_list args, t_option *opt);
+void	ft_printf_unsi_int(va_list args, t_option *opt);
+void	ft_printf_unsi_hex(va_list args, t_option *opt);
+void	ft_printf_point(va_list args, t_option *opt);
+void	ft_printf_percent(t_option *opt);
+void	ft_check_conversion(const char *format, va_list args, t_option *opt);
+void	ft_putnbr_count(int n, t_option *opt);
+void	ft_putstr_count(const char *s, t_option *opt);
+void	ft_putchar_count(char c, t_option *opt);
+void	ft_put_unsi_count(unsigned int n, t_option *opt);
+void	init_options(t_option *opt);
 
 #	endif
